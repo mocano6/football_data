33eeef338ca6dc851a3086a727d8a17c456310dc
Łukasz Ferszt <lukaszferszt@gmail.com>
2023-12-02 14:11:47 +0100

    style changes

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,12 +1,28 @@
 import { Box, Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
-export const Timer = ({ half, getTime, getReset }: { half: any; getTime?: any, getReset?: any }) => {
+export const Timer = ({
+  half,
+  getTime,
+  getReset,
+}: {
+  half: any;
+  getTime?: any;
+  getReset?: any;
+}) => {
   const [time, setTime] = useState(0);
-  
+
   const [running, setRunning] = useState(false);
   useEffect(() => {
-    let interval:any;
+    if (half === 1) {
+      setTime(time);
+    } else if (half === 2) {
+      setTime(time + 45 * 1000 * 60);
+    }
+  }, [half]);
+
+  useEffect(() => {
+    let interval: any;
     if (running) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime + 1000);
@@ -20,42 +36,55 @@ export const Timer = ({ half, getTime, getReset }: { half: any; getTime?: any, g
     return () => clearInterval(interval);
   }, [running, getTime, time]);
   return (
-      <Box sx={{
-        display: 'flex',
-        alignItems: 'center',
-        width: '100%',
-        justifyContent: 'space-around'
-      }}>
-        <Box
-          sx={{
-            fontFamily: 'Raleway',
-            fontSize: 24,
-            textAlign: 'center',
-            display: 'flex',
-            justifyContent: 'space-between'
-          }}
-        >{("0" + Math.floor((time / 60000))).slice(-2)}:{("0" + Math.floor((time / 1000) % 60)).slice(-2)}
-        </Box>
-          <Button
-          size="large"
-          variant="contained"
-          color={running ? 'error' : 'success'}
-          onClick={() => running ? setRunning(false) : setRunning(true)}
-          sx={{
-            margin: 0.1
-          }}>
-            {running ? 'Stop' : 'Start'}
-          </Button>
-          <Button
-          size="large"
-          sx={{
-            margin: 0.1
-          }} variant="contained" color="secondary" onClick={
-            () => {
-              setTime(0)
-              getReset(0)
-            }
-            }>Reset</Button>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        width: "100%",
+        justifyContent: "space-around",
+      }}
+    >
+      <Box
+        sx={{
+          fontFamily: "Raleway",
+          fontSize: 24,
+          textAlign: "center",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        {("0" + Math.floor(time / 60000)).slice(-2)}:
+        {("0" + Math.floor((time / 1000) % 60)).slice(-2)}
       </Box>
+      <Button
+        size="large"
+        variant="contained"
+        color={running ? "error" : "success"}
+        onClick={() => (running ? setRunning(false) : setRunning(true))}
+        sx={{
+          margin: 0.1,
+        }}
+      >
+        {running ? "Stop" : "Start"}
+      </Button>
+      <Button
+        size="large"
+        sx={{
+          margin: 0.1,
+        }}
+        variant="contained"
+        color="secondary"
+        onClick={() => {
+          if (half === 2) {
+            setTime(45 * 1000 * 60);
+          } else if (half === 1) {
+            setTime(0);
+          }
+          getReset(0);
+        }}
+      >
+        Reset
+      </Button>
+    </Box>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,37 +1,46 @@
 import React, { useEffect, useState } from "react";
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
 import { Timer } from "../../components/Timer/Timer";
+import { Button } from "@mui/material";
+import { GridSize } from "@mui/material/Grid";
+import { ButtonProps } from "@mui/material/Button";
 
 export const Home = () => {
   const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#f1f1f1',
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#f1f1f1",
     ...theme.typography.body2,
     padding: theme.spacing(5),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   }));
-  const changeToPercentage = (number:number) => (number * 100).toFixed(2)
+  const changeToPercentage = (number: number) => (number * 100).toFixed(0);
 
   const [half, setHalf] = useState(1);
   const [time, getTime] = useState<number>(0);
   const [reset, getReset] = useState<any>(1);
 
   useEffect(() => {
-    if (reset===0) {
-      setHalf1(clearHalf)
+    if (reset === 0) {
+      setHalf1(clearHalf);
     }
-  }, [reset])
-  
+  }, [reset]);
+
   useEffect(() => {
     if (half1.possessionTeam === 1) {
-      setHalf1((prevHalf1) => ({ ...prevHalf1, possession1: prevHalf1.possession1 + 1 }))
+      setHalf1((prevHalf1) => ({
+        ...prevHalf1,
+        possession1: prevHalf1.possession1 + 1,
+      }));
     } else if (half1.possessionTeam === 2) {
-      setHalf1((prevHalf1) => ({ ...prevHalf1, possession2: prevHalf1.possession2 + 1 }))
+      setHalf1((prevHalf1) => ({
+        ...prevHalf1,
+        possession2: prevHalf1.possession2 + 1,
+      }));
     }
-  }, [time])
+  }, [time]);
   const clearHalf = {
     possessionTeam: 0,
     possession1: 0,
@@ -44,61 +53,218 @@ export const Home = () => {
     Tilt2: 0,
     Rec1: 0,
     Rec2: 0,
-  }
+  };
   const [half1, setHalf1] = useState(clearHalf);
   const [half2, setHalf2] = useState(clearHalf);
 
-  return (
-    <Box sx={{ width: '100%', marginTop: '10px', mouse: 'pointer' }}>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}} item xs={12}>
-          <Timer half={half} getTime={(newTime: any) => getTime(newTime)} getReset={(isReset: any) => getReset(isReset)}/>
-        </Grid>
-        <Grid item xs={12}>
-          <Item  onClick={() => setHalf((prevHalf) => (prevHalf === 1 ? 2 : 1))}>
-            {half} Half
-          </Item>
-        </Grid>
-        <Grid sx={{display: 'flex'}} item xs={6}>
-          <Item sx={half1.possessionTeam === 1 ? {background: '#0074BD', color: 'white'} : null} onClick={()=>setHalf1((prevHalf1) => ({ ...prevHalf1, possessionTeam: prevHalf1.possessionTeam = 1}))}>
-            Possession Miedź ({changeToPercentage(Number(half1.possession1/(half1.possession1 + half1.possession2)))}%)
-          </Item>
-        </Grid>
-        <Grid sx={{display: 'flex'}} item xs={6}>
-          <Item sx={half1.possessionTeam === 2 ? {background: '#0074BD', color: 'white'} : null} onClick={()=>setHalf1((prevHalf1) => ({ ...prevHalf1, possessionTeam: prevHalf1.possessionTeam = 2}))}>
-            Possession Opponent ({changeToPercentage(Number(half1.possession2/(half1.possession1 + half1.possession2)))}%)
-          </Item>
-        </Grid>
-        <Grid sx={{display: 'flex'}} item xs={6}>
-          <Item onClick={()=>setHalf1((prevHalf1) => ({ ...prevHalf1, pass1: prevHalf1.pass1 + 1 }))}>
-            Pass Miedź ({half1.pass1}) ({changeToPercentage(Number(half1.pass1/(half1.pass1 + half1.badPass1)))}%)</Item>
-        </Grid>
-        <Grid sx={{display: 'flex'}} item xs={6}>
-          <Item onClick={()=>setHalf1((prevHalf1) => ({ ...prevHalf1, pass2: prevHalf1.pass2 + 1 }))}>
-            Pass Opponent ({half1.pass2})({changeToPercentage(Number(half1.pass2/(half1.pass2 + half1.badPass2)))}%)</Item>
+  const buttons = [
+    {
+      key: "half",
+      style: {},
+      innerElement: "",
+      text: half + " Half",
+      onClick: () => setHalf((prevHalf) => (prevHalf === 1 ? 2 : 1)),
+      gridXS: 12,
+      variant: "outlined",
+    },
+    {
+      key: "possession1",
+      style: {},
+      innerElement: "",
+      text: `Possession 1 (${changeToPercentage(
+        Number(half1.possession1 / (half1.possession1 + half1.possession2))
+      )}%)`,
+      onClick: () =>
+        setHalf1((prevHalf1) => ({
+          ...prevHalf1,
+          possessionTeam: (prevHalf1.possessionTeam = 1),
+        })),
+      gridXS: 6,
+      variant: half1.possessionTeam === 1 ? "contained" : "outlined",
+    },
+    {
+      key: "possession2",
+      style: {},
+      innerElement: "",
+      text: `Possession 2 (${changeToPercentage(
+        Number(half1.possession2 / (half1.possession1 + half1.possession2))
+      )}%)`,
+      onClick: () =>
+        setHalf1((prevHalf1) => ({
+          ...prevHalf1,
+          possessionTeam: (prevHalf1.possessionTeam = 2),
+        })),
+      gridXS: 6,
+      variant: half1.possessionTeam === 2 ? "contained" : "outlined",
+    },
+    {
+      key: "pass1",
+      style: {},
+      innerElement: "",
+      text: `
+      Pass (${half1.pass1})
+      \n(${changeToPercentage(
+        Number(half1.pass1 / (half1.pass1 + half1.badPass1))
+      )}%)`,
+      onClick: () =>
+        setHalf1((prevHalf1) => ({
+          ...prevHalf1,
+          pass1: prevHalf1.pass1 + 1,
+        })),
+      gridXS: 3,
+      variant: "outlined",
+      color: "success",
+    },
+    {
+      key: "passBad1",
+      style: {},
+      innerElement: "",
+      text: `
+      Bad
+      \n(${half1.badPass1})`,
+      onClick: () =>
+        setHalf1((prevHalf1) => ({
+          ...prevHalf1,
+          badPass1: prevHalf1.badPass1 + 1,
+        })),
+      gridXS: 3,
+      variant: "contained",
+      color: "error",
+    },
+    {
+      key: "pass2",
+      style: {},
+      innerElement: "",
+      text: `
+      Pass
+      \n(${half1.pass2})
+      \n(${changeToPercentage(
+        Number(half1.pass2 / (half1.pass2 + half1.badPass2))
+      )}%)`,
+      onClick: () =>
+        setHalf1((prevHalf1) => ({
+          ...prevHalf1,
+          pass2: prevHalf1.pass2 + 1,
+        })),
+      gridXS: 3,
+      variant: "outlined",
+      color: "success",
+    },
+    {
+      key: "passBad2",
+      style: {},
+      innerElement: "",
+      text: `Bad 2 (${half1.badPass2})`,
+      onClick: () =>
+        setHalf1((prevHalf1) => ({
+          ...prevHalf1,
+          badPass2: prevHalf1.badPass2 + 1,
+        })),
+      gridXS: 3,
+      variant: "contained",
+      color: "error",
+    },
+    {
+      key: "tilt1",
+      style: {},
+      innerElement: "",
+      text: `Tilt Pass (${half1.Tilt1}) (
+            ${changeToPercentage(
+              Number(half1.Tilt1 / (half1.Tilt1 + half1.Tilt2))
+            )}
+            %)`,
+      onClick: () =>
+        setHalf1((prevHalf1) => ({
+          ...prevHalf1,
+          Tilt1: prevHalf1.Tilt1 + 1,
+          pass1: prevHalf1.pass1 + 1,
+        })),
+      gridXS: 6,
+      variant: "contained",
+      color: "success",
+    },
+    {
+      key: "tilt2",
+      style: {},
+      innerElement: "",
+      text: `Tilt Pass (${half1.Tilt2}) (
+            ${changeToPercentage(
+              Number(half1.Tilt2 / (half1.Tilt1 + half1.Tilt2))
+            )}
+            %)`,
+      onClick: () =>
+        setHalf1((prevHalf1) => ({
+          ...prevHalf1,
+          Tilt2: prevHalf1.Tilt2 + 1,
+          pass2: prevHalf1.pass2 + 1,
+        })),
+      gridXS: 6,
+      variant: "contained",
+      color: "success",
+    },
+    {
+      key: "5sReg1",
+      style: {},
+      innerElement: "",
+      text: `5s Reg (${half1.Rec1})`,
+      onClick: () =>
+        setHalf1((prevHalf1) => ({
+          ...prevHalf1,
+          Rec1: prevHalf1.Rec1 + 1,
+        })),
+      gridXS: 6,
+      variant: "outlined",
+      color: "secondary",
+    },
+    {
+      key: "5sReg2",
+      style: {},
+      innerElement: "",
+      text: `5s Reg (${half1.Rec2})`,
+      onClick: () =>
+        setHalf1((prevHalf1) => ({
+          ...prevHalf1,
+          Rec2: prevHalf1.Rec2 + 1,
+        })),
+      gridXS: 6,
+      variant: "outlined",
+      color: "secondary",
+    },
+  ];
 
+  return (
+    <Box sx={{ width: "100%", marginTop: "10px", mouse: "pointer" }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          item
+          xs={12}
+        >
+          <Timer
+            half={half}
+            getTime={(newTime: any) => getTime(newTime)}
+            getReset={(isReset: any) => getReset(isReset)}
+          />
         </Grid>
-        <Grid sx={{display: 'flex'}} item xs={6}>
-          <Item onClick={()=>setHalf1((prevHalf1) => ({ ...prevHalf1, badPass1: prevHalf1.badPass1 + 1 }))}>
-            Pass bad Miedź ({half1.badPass1})</Item>
-        </Grid>
-        <Grid sx={{display: 'flex'}} item xs={6}>
-          <Item onClick={()=>setHalf1((prevHalf1) => ({ ...prevHalf1, badPass2: prevHalf1.badPass2 + 1 }))}>
-            Pass bad Opponent ({half1.badPass2})</Item>
-        </Grid>
-        <Grid sx={{display: 'flex'}} item xs={6}>
-          <Item onClick={()=>setHalf1((prevHalf1) => ({ ...prevHalf1, Tilt1: prevHalf1.Tilt1 + 1 }))}>Pass Opp. Half Miedź ({half1.Tilt1}) ({changeToPercentage(Number(half1.Tilt1/(half1.Tilt1 + half1.Tilt2)))}%)</Item>
-        </Grid>
-        <Grid sx={{display: 'flex'}} item xs={6}>
-          <Item onClick={()=>setHalf1((prevHalf1) => ({ ...prevHalf1, Tilt2: prevHalf1.Tilt2 + 1 }))}>Pass Opp. Half Opponent ({half1.Tilt2}) ({changeToPercentage(Number(half1.Tilt2/(half1.Tilt1 + half1.Tilt2)))}%)</Item>
-        </Grid>
-        <Grid sx={{display: 'flex'}} item xs={6}>
-          <Item onClick={()=>setHalf1((prevHalf1) => ({ ...prevHalf1, Rec1: prevHalf1.Rec1 + 1 }))}>5 sec Regain Miedź({half1.Rec1})</Item>
-        </Grid>
-        <Grid sx={{display: 'flex'}} item xs={6}>
-          <Item onClick={()=>setHalf1((prevHalf1) => ({ ...prevHalf1, Rec2: prevHalf1.Rec2 + 1 }))}>
-            5 sec Regain Opponent ({half1.Rec2})</Item>
-        </Grid>
+        {buttons.map((btn) => (
+          <Grid item xs={btn.gridXS as GridSize} key={btn.key}>
+            <Button
+              sx={{
+                width: "-webkit-fill-available",
+                height: "-webkit-fill-available",
+              }}
+              color={btn?.color as ButtonProps["color"]}
+              onClick={btn.onClick}
+              variant={btn.variant as ButtonProps["variant"]}
+            >
+              {btn.text}
+            </Button>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
